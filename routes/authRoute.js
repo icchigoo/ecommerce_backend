1c7574ed54a26dc9f6eb87d69f5dcb8702008fc2
icchigoo <icchigoo12@gmail.com>
2023-05-23 15:06:12 +0545

    login, register and fetch on mongodb

---
+++ routes/authRoute.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const {
+    createUser,
+    loginUserCtrl,
+    getallUser,
+    } = require("../controller/userCtrl");
+const router = express.Router();
+
+
+router.post("/register", createUser);
+router.post("/login", loginUserCtrl);
+router.get("/all-users", getallUser);
+module.exports = router ;
